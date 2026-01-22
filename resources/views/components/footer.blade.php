631c0b30e0929c3fa161b7fcf630d7bca620c443
Oleksha <43583583+Oleksha@users.noreply.github.com>
2026-01-22 11:33:20 +0400

    Create footer.blade.php

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="bg-body-tertiary border-top py-3">
+    <div class="container text-center">
+        <span class="text-muted">
+            Разработано ИТО ОП «Тольятти» &copy; 2021–{{ date('Y') }}
+        </span>
+    </div>
+</footer>
