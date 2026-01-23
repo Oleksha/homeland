abe37e3e6a6793133047c23d7c7e807a0f8cd65d
Oleksha <43583583+Oleksha@users.noreply.github.com>
2026-01-23 11:29:46 +0400

    Create Navigation.php

---
+++ app/View/Components/Navigation.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\View\Components;
+
+use Closure;
+use Illuminate\View\Component;
+use Illuminate\Contracts\View\View;
+
+class Navigation extends Component
+{
+    /**
+     * Create a new component instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function render(): View|Closure|string
+    {
+        return view('components.navigation');
+    }
+}
