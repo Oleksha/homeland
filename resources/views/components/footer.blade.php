51f813684a8a98b863483d03b502147e31caa4a1
Oleksha <43583583+Oleksha@users.noreply.github.com>
2026-01-23 11:31:37 +0400

    Update footer.blade.php

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="bg-body-tertiary border-top py-3">
+<footer class="mt-auto bg-body-tertiary border-top py-3">
     <div class="container text-center">
         <span class="text-muted">
             Разработано ИТО ОП «Тольятти» &copy; 2021–{{ date('Y') }}
