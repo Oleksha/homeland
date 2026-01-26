80c6fdaa2f054d9ffe9c047de75d49a9d61cbd57
Oleksha <43583583+Oleksha@users.noreply.github.com>
2026-01-26 15:13:52 +0400

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', fn () => redirect('/vats'))->name('home');
 
+// Справочники
+Route::get('/directories', Controllers\DirectoryController::class)
+    ->name('directories.index');
+
 Route::resource('vats', Controllers\VatController::class)
     ->except(['show']);
 Route::resource('contractor-types', Controllers\ContractorTypeController::class)
     ->except(['show']);
+
 // Контрагенты
 Route::get('contractors/archive', [Controllers\ContractorController::class, 'archive'])
     ->name('contractors.archive');
