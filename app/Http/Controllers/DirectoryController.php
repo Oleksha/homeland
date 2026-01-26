5fccb502b348a29c3548291c61c054697e7587ec
Oleksha <43583583+Oleksha@users.noreply.github.com>
2026-01-26 15:07:40 +0400

    Create DirectoryController.php

---
+++ app/Http/Controllers/DirectoryController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\Directory\DirectoryListService;
+
+class DirectoryController extends Controller
+{
+    public function __invoke(DirectoryListService $service)
+    {
+        return view('directories.index', [
+            'directories' => $service->get(),
+            'breadcrumbs' => [
+                ['title' => 'Справочники'],
+            ],
+        ]);
+    }
+}
