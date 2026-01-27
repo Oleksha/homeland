62f643660ed874ba7b372577d713cf2c887182bc
Oleksha <43583583+Oleksha@users.noreply.github.com>
2026-01-27 12:43:10 +0400

    Create Receipt.php

---
+++ app/Models/Receipt.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\ReceiptType;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Receipt extends Model
+{
+    protected $fillable = [
+        'date',
+        'number',
+        'type',
+        'contractor_id',
+        'document_number',
+        'document_date',
+        'note',
+        'status',
+        'total_amount',
+        'total_vat',
+    ];
+
+    protected $casts = [
+        'date' => 'date',
+        'document_date' => 'date',
+        'type' => ReceiptType::class,
+        'total_amount' => 'decimal:2',
+        'total_vat' => 'decimal:2',
+        'status' => 'boolean',
+    ];
+
+    public function items(): HasMany
+    {
+        return $this->hasMany(ReceiptItem::class);
+    }
+
+    public function contractor(): BelongsTo
+    {
+        return $this->belongsTo(Contractor::class);
+    }
+}
