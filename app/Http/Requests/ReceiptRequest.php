f76f39ac7b91e121878a0da8943d224106c457b6
Oleksha <43583583+Oleksha@users.noreply.github.com>
2026-01-27 12:40:04 +0400

    Create ReceiptRequest.php

---
+++ app/Http/Requests/ReceiptRequest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\DTO\Receipt\ReceiptDTO;
+use Illuminate\Foundation\Http\FormRequest;
+
+class ReceiptRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'date' => ['required', 'date'],
+            'number' => ['required', 'string', 'max:50'],
+            'type' => ['required'],
+            'contractor_id' => ['required', 'exists:contractors,id'],
+
+            'document_number' => ['nullable', 'string', 'max:50'],
+            'document_date' => ['nullable', 'date'],
+            'note' => ['nullable', 'string'],
+
+            'items' => ['required', 'array', 'min:1'],
+            'items.*.name' => ['required', 'string'],
+            'items.*.quantity' => ['required', 'numeric', 'min:0.0001'],
+            'items.*.price' => ['required', 'numeric', 'min:0'],
+            'items.*.vat_id' => ['required', 'exists:vats,id'],
+        ];
+    }
+
+
+    public function validatedDTO(): ReceiptDTO
+    {
+        return ReceiptDTO::fromRequest($this->validated());
+    }
+}
