6dbe075462809f8cdab78aeb123839eedb41c98c
Oleksha <43583583+Oleksha@users.noreply.github.com>
2026-01-27 12:44:18 +0400

    Create ReceiptItem.php

---
+++ app/Models/ReceiptItem.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class ReceiptItem extends Model
+{
+    protected $fillable = [
+        'receipt_id',
+        'name',
+        'quantity',
+        'price',
+        'amount',
+        'vat_id',
+        'vat_amount',
+        'total_amount',
+    ];
+
+    public function receipt(): BelongsTo
+    {
+        return $this->belongsTo(Receipt::class);
+    }
+
+    public function vat(): BelongsTo
+    {
+        return $this->belongsTo(Vat::class);
+    }
+}
