af094e9c5a08d101f71b60286cda52074902689a
Oleksha <43583583+Oleksha@users.noreply.github.com>
2026-01-27 12:55:05 +0400

    Create 2026_01_26_114717_create_receipt_items_table.php

---
+++ database/migrations/2026_01_26_114717_create_receipt_items_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('receipt_items', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('receipt_id')
+                ->constrained()
+                ->cascadeOnDelete();
+
+            $table->string('name');
+
+            $table->decimal('quantity', 15, 3);
+            $table->decimal('price', 15, 2);
+
+            $table->decimal('amount', 15, 2);        // quantity * price
+            $table->foreignId('vat_id')->nullable()->constrained();
+
+            $table->decimal('vat_amount', 15, 2)->default(0);
+            $table->decimal('total_amount', 15, 2);  // amount + vat
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('receipt_items');
+    }
+};
