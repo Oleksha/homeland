2610d49df89b7978cf98728a9417d92e16ac7192
Oleksha <43583583+Oleksha@users.noreply.github.com>
2026-01-30 15:17:07 +0400

    Update ContractorType.php

--- app/Domains/ContractorType/Models/ContractorType.php
+++ app/Domains/ContractorType/Models/ContractorType.php
@@ -2,11 +2,18 @@
 
 namespace App\Domains\ContractorType\Models;
 
+use App\Domains\Contractor\Models\Contractor;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class ContractorType extends Model
 {
     protected $fillable = [
         'name',
     ];
+
+    public function contractors(): HasMany
+    {
+        return $this->hasMany(Contractor::class);
+    }
 }
