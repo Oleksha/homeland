8cc6aa680bb526f868934ced9d5df8e9bf028897
Oleksha <deryabino@gmail.com>
2026-01-31 13:45:50 +0400

    Исправление

--- app/Domains/Vat/Actions/GetActiveVats.php
+++ app/Domains/Vat/Actions/GetActiveVats.php
@@ -1,1 +1,17 @@
+<?php
 
+namespace App\Domains\Vat\Actions;
+
+use App\Domains\Vat\Models\Vat;
+use Illuminate\Support\Collection;
+use App\Support\Action;
+
+class GetActiveVats extends Action
+{
+    public function __invoke(): Collection
+    {
+        return Vat::active()
+            ->orderBy('rate')
+            ->get();
+    }
+}
