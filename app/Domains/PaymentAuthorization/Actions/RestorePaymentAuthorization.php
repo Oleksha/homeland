2931928324d7e2e1c884958c2745e356531d8e65
Oleksha <43583583+Oleksha@users.noreply.github.com>
2026-02-03 14:59:32 +0400

    Create RestorePaymentAuthorization.php

---
+++ app/Domains/PaymentAuthorization/Actions/RestorePaymentAuthorization.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Domains\PaymentAuthorization\Actions;
+
+use App\Domains\PaymentAuthorization\Models\PaymentAuthorization;
+use App\Support\Action;
+
+class RestorePaymentAuthorization extends Action
+{
+    public function handle(int $id): void
+    {
+        PaymentAuthorization::withTrashed()
+            ->findOrFail($id)
+            ->restore();
+    }
+}
